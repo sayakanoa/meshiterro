89ee0be85e1019308e5c887a89499f816e22f380
sayakanoa <reiluv75418@gmail.com>
2021-09-15 09:33:54 +0000

    画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ad465a23a2015d43f20d7ad748fd2ce1df0fcd55e3c0414a5f1eab6613d09e024544af47a2fb0832f21bf28e521c0164074391cdd2c369bfa31ec41a7e8af91a'
